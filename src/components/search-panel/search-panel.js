6f6cc1874674f6e740b10c8224d5753ec5b61ab0
savka-olia <tokarchykolia@gmail.com>
2022-10-14 12:42:30 +0300

    search panel filtering

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import "./search-panel.css";
 
-const SearchPanel = () => {
+export default class SearchPanel extends Component {
 
-  return (
-    <input type="text"
-           placeholder="type to search"
-           className="search-input"
-  	/>
-  );
-};
+  state = {
+    term: ''
+  };
 
-export default SearchPanel;
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  }
+
+  render(){
+    return (
+      <input type="text"
+             placeholder="type to search"
+             className="search-input"
+             value={this.state.term} 
+             onChange={this.onSearchChange} />
+    );
+  };
+}
